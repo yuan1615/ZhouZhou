3f23c021426aad2098e42f5b3546ff16466b7243
Xin Yuan <XinYuan1615@gmail.com>
2019-10-22 14:37:57 +0800

    Zhou

---
+++ R/Who_is_ZZ.R
@@ -0,0 +1,17 @@
+#' Zhou Zhou
+#'
+#' Who is Zhou Zhou?
+#' @param printlog logical. Whether to print.
+#' @keywords Zhou Zhou
+#' @export
+#' @examples
+#' Who_is_ZZ()
+
+
+Who_is_ZZ <- function(printlog = TRUE){
+  if(printlog == TRUE){
+    print("Zhouzhou is your queen, she is cute and beautiful and you love her very much.")
+  }else{
+    print("lalalalala")
+  }
+}
